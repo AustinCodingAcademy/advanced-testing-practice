92afdb422b26e783725b8e8b0b2fb06a4f291136
Dan Putnam <daniel.a.putnam@gmail.com>
2017-07-10 20:52:14 -0500

    initial

--- src/tests/array-functions.test.js
+++ src/tests/array-functions.test.js
@@ -1,4 +1,4 @@
-import {map,filter,find,findLast} from "../services/array-functions";
+import {map,filter,find,findLast,head} from "../services/array-functions";
 const names = ["Jon","Bob","Ted","Barney","Lilly","Robin","Saul","Axe"];
 const myNumbers = [4,3,55,22,99,1913,7,5,4,2,1];
 
@@ -44,6 +44,15 @@ describe("sort", () => {
 
 //filter should return an array with names of length 3
 //["Jon","Bob","Ted","Axe"]
+describe("filter", () => {
+  it("should return an array with names of length 3", () => {
+    let filteredArray = filter(names, findThree);
+
+    expect(filteredArray).toEqual([
+      "Jon","Bob","Ted","Axe"
+    ]);
+  });
+});
 
 //find should find one name of "Barney"
 
@@ -53,5 +62,3 @@ describe("sort", () => {
 //["Axe","Saul","Robin","Lilly","Barney","Ted","Bob","Jon"]
 //tail should return all elements in an array except the first one
 //[Bob","Ted","Barney","Lilly","Robin","Saul","Axe"];
-
-
