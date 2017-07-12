d320a42a491c6d84676eda6154d0c0f589185377
mymonstermaggie <maggie@remote-com.com>
2017-07-11 21:22:45 -0500

    add passing arrayFunctions and tests

--- src/services/array-functions.js
+++ src/services/array-functions.js
@@ -1,40 +1,50 @@
 
 //in the function map, create a new array and store in a variable
-//loop theArray and call the fnc for each thing in the array, 
+//loop theArray and call the fnc for each thing in the array,
 //     passing in the item from the current loop into the call to fnc
 //add the returned value from fnc to the new array
 //return the new array
 export function map(theArray, fnc){
-
+  let newArray = [];
+  newArray = theArray.map(fnc);
+  return newArray;
 }
 
 //create a new array
-//loop theArray and call the fnc for each thing in the array, 
+//loop theArray and call the fnc for each thing in the array,
 //     passing in the item from the current loop
 //fnc will return true or false, if true add the item to the new array else do not
 //return the new array
 export function filter(theArray, fnc){
-
+  let newArray = [];
+  newArray = theArray.filter(fnc);
+  return newArray;
 }
 
 
-//loop theArray and call the fnc for each thing in the array, 
+//loop theArray and call the fnc for each thing in the array,
 //     passing in the item from the current loop
-//fnc will return true or false, if true return the item 
+//fnc will return true or false, if true return the item
 //return null
 export function find(theArray, fnc){
-
+  let found = null;
+  found = theArray.find(fnc);
+  return found;
 }
 
 
 //return the last item in theArray
 export function findLast(theArray){
-
+  let found = null;
+  found = theArray[theArray.length - 1];
+  return found;
 }
 
 //return the first element of the array
 export function head(theArray){
-
+  let found = null;
+  found = theArray[0];
+  return found;
 }
 
 //create a new array
@@ -42,7 +52,9 @@ export function head(theArray){
 //add the item from each loop to the new array
 //return the new array
 export function reverse(theArray){
-
+  let newArray = [];
+  newArray = theArray.reverse();
+  return newArray;
 }
 
 //create a new array
@@ -50,7 +62,9 @@ export function reverse(theArray){
 //add the item from each loop to the new array except the first item
 //return the new array
 export function tail(theArray){
-
+  let newArray = [];
+  newArray = theArray.slice(1);
+  return newArray;
 }
 
 //implement the most basic sorting algorithm there is
@@ -63,6 +77,18 @@ export function tail(theArray){
 //if a swap is done set it to true
 //after each for loop check the variable, if true, continue the while loop
 //if false return theArray
-export function sort(theArray){
-
+export function sort(arr){
+  let swapped;
+  do {
+    swapped = false;
+    for(var i=0; i<arr.length; i++){
+      if(arr[i] > arr[i+1]){
+        let temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
 }
