c77f9fb606c5774ec806d1d9b0b5ce7c31ae8b37
Jon Woo <jw56578@gmail.com>
2017-10-04 18:39:54 -0500

    changed stuff

--- src/services/array-functions.js
+++ src/services/array-functions.js
@@ -5,7 +5,13 @@
 //add the returned value from fnc to the new array
 //return the new array
 export function map(theArray, fnc){
-
+    var newArray = [];
+    for(var i = 0 ; i < theArray.length; i++){
+        var currentItem = theyArray[i];
+        var returneditem = fnc(currentItem);
+        newArray[i]= (returneditem);
+    }
+    return newArray;
 }
 
 //create a new array
