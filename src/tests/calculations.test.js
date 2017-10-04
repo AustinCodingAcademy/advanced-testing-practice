32775c2066c803f5d5cf1ac4af0bd1637265504d
Kyle <rhodestraveler@gmail.com>
2017-10-04 18:15:01 -0500

    array-functions testing homework

--- src/tests/calculations.test.js
+++ src/tests/calculations.test.js
@@ -5,3 +5,21 @@ describe("add", () => {
     expect(add(1, 2)).toBe(3);
   });
 });
+
+describe("multiply", () => {
+  it("should multiply 2 and 3 and return 6", () => {
+    expect(multiply(2,3)).toBe(6);
+  });
+})
+
+describe("subract", () => {
+  it("should subtract 1 from 3 and return 2", () => {
+    expect(subtract(3,1)).toBe(2);
+  });
+})
+
+describe("divide", () => {
+  it("should divide 6 by 3 and return 2", () => {
+    expect(divide(6,3)).toBe(2);
+  });
+})
