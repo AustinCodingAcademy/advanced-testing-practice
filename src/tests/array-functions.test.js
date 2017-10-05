0a99ed6f3a78247e33c7f3d4df63b937c9344c08
Jason Alexander <jasonalexander@059365743453.azz.com>
2017-10-05 18:21:45 -0500

    Added Test Functions

--- src/tests/array-functions.test.js
+++ src/tests/array-functions.test.js
@@ -44,14 +44,28 @@ describe("sort", () => {
 
 //filter should return an array with names of length 3
 //["Jon","Bob","Ted","Axe"]
+describe("filter", () => {
+  it("should filter out specific names", () => {
+    expect(filter(names,findThree)).toEqual([
+      "Jon",
+      "Bob",
+      "Ted",
+      "Axe"
+    ]);
+  });
+});
 
 //find should find one name of "Barney"
+describe("find", () => {
+  it("should find a specific name", () => {
+    expect(find(names,findBarney)).toEqual("Barney");
+  });
+});
 
 //findLast should find the last name of "Axe"
 
 //reverse should return an array with the elements in the opposite order
 //["Axe","Saul","Robin","Lilly","Barney","Ted","Bob","Jon"]
+
 //tail should return all elements in an array except the first one
 //[Bob","Ted","Barney","Lilly","Robin","Saul","Axe"];
-
-
