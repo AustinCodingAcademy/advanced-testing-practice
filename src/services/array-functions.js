bd55fcf156349aaecc5a00415f294f4b9331c97c
Mac User <macuser@macs-mbp.gateway.pace.com>
2017-10-05 13:48:26 -0700

    add updated calc & array func testing hw files

--- src/services/array-functions.js
+++ src/services/array-functions.js
@@ -1,68 +1,124 @@
 
-//in the function map, create a new array and store in a variable
-//loop theArray and call the fnc for each thing in the array, 
+// in the function map, create a new array and store in a variable
+// loop theArray and call the fnc for each thing in the array,
 //     passing in the item from the current loop into the call to fnc
-//add the returned value from fnc to the new array
-//return the new array
-export function map(theArray, fnc){
-
+// add the returned value from fnc to the new array
+// return the new array
+export function map (theArray, fnc) {
+  let newArray = [];
+  theArray.forEach(function (element) {
+    newArray.push(fnc(element));
+  });
+  return newArray;
 }
 
-//create a new array
-//loop theArray and call the fnc for each thing in the array, 
+// implement the most basic sorting algorithm there is
+// assume the array will always have numbers
+// use a while loop to constantly loop theArray until it is sorted
+// use a for loop to loop theArray
+// look at the current item and the next item, compare them
+// if the items are out of order, swap them
+// initialize a variable that indicates if a swap had to be done, set it to false
+// if a swap is done set it to true
+// after each for loop check the variable, if true, continue the while loop
+// if false return theArray
+
+export function sort (theArray) {
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let i = 1; i < theArray.length; i++) {
+      if (theArray[i - 1] > theArray[i]) {
+        sorted = false;
+        let temp = theArray[i - 1];
+        theArray[i - 1] = theArray[i];
+        theArray[i] = temp;
+      }
+    }
+  }
+  return theArray;
+}
+
+/* ALT SORT using i=0 and array length - 1
+
+export function sort(theArray) {
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < theArray.length-1; i++) {
+      if (theArray[i] > theArray[i+1]) {
+        sorted = false;
+        let temp = theArray[i];
+        theArray[i] = theArray[i + 1];
+        theArray[i + 1] = temp;
+      }
+    }
+  }
+  return theArray;
+}
+*/
+
+// create a new array
+// loop theArray and call the fnc for each thing in the array,
 //     passing in the item from the current loop
-//fnc will return true or false, if true add the item to the new array else do not
-//return the new array
-export function filter(theArray, fnc){
-
+// fnc will return true or false, if true add the item to the new array else do not
+// return the new array
+export function filter (theArray, fnc) {
+  let newArray = [];
+  theArray.forEach(function (element) {
+    if (fnc(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 }
 
-
-//loop theArray and call the fnc for each thing in the array, 
+// loop theArray and call the fnc for each thing in the array,
 //     passing in the item from the current loop
-//fnc will return true or false, if true return the item 
-//return null
-export function find(theArray, fnc){
-
+// fnc will return true or false, if true return the item
+// return null
+export function find (theArray, fnc) {
+  let returnValue = '';
+  for (let i = 0; i < theArray.length; i++) {
+    if (fnc(theArray[i]) === true) {
+      returnValue = theArray[i];
+      return returnValue;
+    } else returnValue = null;
+  }
+  return returnValue;
 }
 
-
-//return the last item in theArray
-export function findLast(theArray){
-
+// return the last item in theArray
+export function findLast (theArray) {
+  return theArray[(theArray.length - 1)];
 }
 
-//return the first element of the array
-export function head(theArray){
-
+// create a new array
+// loop theArray in reverse order
+// add the item from each loop to the new array
+// return the new array
+export function reverse (theArray) {
+  let newArray = [];
+  for (let i = 0; i < theArray.length; i++) {
+    newArray[i] = theArray[(theArray.length - 1) - i];
+  }
+  return newArray;
 }
 
-//create a new array
-//loop theArray in reverse order
-//add the item from each loop to the new array
-//return the new array
-export function reverse(theArray){
-
+// return the first element of the array
+export function head (theArray) {
+  return theArray[0];
 }
 
-//create a new array
-//loop theArray
-//add the item from each loop to the new array except the first item
-//return the new array
-export function tail(theArray){
+// create a new array
+// loop theArray
+// add the item from each loop to the new array except the first item
+// return the new array
 
-}
-
-//implement the most basic sorting algorithm there is
-//assume the array will always have numbers
-//use a while loop to constantly loop theArray until it is sorted
-//use a for loop to loop theArray
-//look at the current item and the next item, compare them
-//if the items are out of order, swap them
-//initialize a variable that indicates if a swap had to be done, set it to false
-//if a swap is done set it to true
-//after each for loop check the variable, if true, continue the while loop
-//if false return theArray
-export function sort(theArray){
-
+export function tail (theArray) {
+  let newArray = [];
+  for (let i = 1; i < theArray.length; i++) {
+    newArray.push(theArray[i]);
+  }
+  return newArray;
 }
--- src/tests/calculations.test.js
+++ src/tests/calculations.test.js
@@ -1,7 +1,33 @@
-import {add, subtract, multiply,divide} from "../services/calculations";
+//importing code from other files
+import {add, subtract, multiple,divide} from "../services/calculations";
+
+// describe - what are you Testing
+// it - it should do this
+// both are keywords in testing library
+// jest follows english words
 
 describe("add", () => {
   it("should add 1 and 2 and return 3", () => {
-    expect(add(1, 2)).toBe(3);
+    var result = add(1,2);
+    expect(result).toBe(3);
+  });
+});
+
+describe("subtract", () => {
+  it("should subtract 1 from 3 and return 2", () => {
+    var result = subtract(3,1);
+    expect(result).toBe(2);
+  });
+});
+describe("multiple", () => {
+  it("should multiply 2 and 3 and return 6", () => {
+    var result = multiple(2,3);
+    expect(result).toBe(6);
+  });
+});
+describe("divide", () => {
+  it("should divide 21 by 3 and return 7", () => {
+    var result = divide(21,3);
+    expect(result).toBe(7);
   });
 });
