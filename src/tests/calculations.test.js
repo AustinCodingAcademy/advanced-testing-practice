2746650c3ce152aed41c25d994c36888aed747a2
Jordon R <reese.jordon@gmail.com>
2018-01-24 12:41:36 -0600

    Calculations test for add, subtract, multiply, and divide complete

--- src/tests/calculations.test.js
+++ src/tests/calculations.test.js
@@ -1,7 +1,25 @@
-import {add, subtract, multiply,divide} from "../services/calculations";
+import {add, subtract, multiple,divide} from "../services/calculations";
 
 describe("add", () => {
   it("should add 1 and 2 and return 3", () => {
     expect(add(1, 2)).toBe(3);
+  });
+});
+
+describe("subtract", () => {
+  it("should subtract 1 from 3 and return 2", () => {
+    expect(subtract(3,1)).toBe(2);
+  })
+});
+
+describe("multiply", () => {
+  it("should multiply 2 and 3 and return 6", () => {
+    expect(multiple(2,3)).toBe(6);
+  });
+});
+
+describe("divide", () => {
+  it("should divide 2 from 6 and return 3", () => {
+    expect(divide(6,2)).toBe(3);
   });
 });
